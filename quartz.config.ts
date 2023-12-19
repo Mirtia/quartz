89369353b0af41989c30d84b197533a3d69a4990
Myrsini Gkolemi <mirtoseri@gmail.com>
2023-12-19 02:03:47 +0100

    change title, it worked fewww

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🔄 Reversing Journal",
+    pageTitle: "📖 Blogs",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
